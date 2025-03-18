cab95fd4277a5e243b191ebda3229b71cc6efe2e
pranjal-prateek <pranjalprateek0903@gmail.com>
2025-03-18 18:21:22 +0530

    feat: added throtlling

--- Pollyfils/Throttling/throttling.js
+++ Pollyfils/Throttling/throttling.js
@@ -4,16 +4,18 @@ function throttle(fn, limit) {
   return function (...args) {
     const now = Date.now();
     if (now - lastCall < limit) {
-      return;
+      return; // Ignore if within the limit time
     }
-    last = now;
-    return cb(...args);
+    lastCall = now;
+    return fn(...args); // Execute the function
   };
 }
+
 function logMessage() {
   console.log("Throttled function executed:", new Date().toLocaleTimeString());
 }
 
 const throttledLog = throttle(logMessage, 2000);
 
-window.addEventListener("scroll", throttledLog); // Logs message at most once every 2 seconds
+// Throttled on scroll
+window.addEventListener("scroll", throttledLog);
